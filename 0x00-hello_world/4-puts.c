73cc754a705b71bb2aede969681e1a75fb5ce083
Snothando96 <snothandomhlwazi@gmail.com>
2023-02-20 13:53:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #Include <stdio.h>
+/**
+ *main - Programming is like a mulitilingual puzzle
+ *Return: Always 0 (Success)
+ */
 
-int main( void) {
-	puts("/"Programming is like building a multilingual puzzle");
-	
-	return 0
-}
+int main (void)
+{
